73ef60b30005c2af70317d7fd6783f1fb1111d92
Ekramul Islam Sumon <mmsumon799@gmail.com>
2023-01-10 12:12:45 +0600

    updated AmenityRepository file

--- app/Repositories/AmenityRepository.php
+++ app/Repositories/AmenityRepository.php
@@ -9,30 +9,54 @@ use Illuminate\Http\Request;
 
 class AmenityRepository implements AmenityRepositoryInterface
 {
+    /**
+     * @param Request $request
+     * @return LengthAwarePaginator
+     */
     public function all(Request $request): LengthAwarePaginator
     {
         $query = Amenity::query();
         if ($request->filled('search')) {
             $query->where('name', 'LIKE', "%{$request->search}%");
         }
+        if (!$request->filled('per_page')) {
+            $request->request->add(['per_page' => $query->count()]);
+        }
         return $query->latest()->paginate($request->get('per_page', config('constant.pagination')));
     }
 
+    /**
+     * @param $data
+     * @return mixed
+     */
     public function store($data)
     {
         return Amenity::create($data);
     }
 
+    /**
+     * @param $model
+     * @return mixed
+     */
     public function find($model)
     {
         return $model;
     }
 
+    /**
+     * @param $model
+     * @param $data
+     * @return mixed
+     */
     public function update($model, $data)
     {
         return $model->update($data);
     }
 
+    /**
+     * @param $model
+     * @return void
+     */
     public function destroy($model)
     {
         $model->delete();
